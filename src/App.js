6f3b8210aee1d2ba16a7604a3f24d8a8ff8d4ffc
fmaulanaarif@gmail.com <fmaulanaarif@gmail.com>
2022-10-11 12:58:49 +0700

    Basic Project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Reviews from './Reviews'
+
+function App() {
+  return (
+    <>
+      <main>
+        <section>
+          <div className='container'>
+            <div className='title'>
+              <h2>Review Contact</h2>
+              <div className='underline'></div>
+              <Reviews />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
